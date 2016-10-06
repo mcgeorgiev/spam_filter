2fd7939363d41c37109830023a8beff5e1817dc8
mcgeorgiev <michael_georgiev@hotmail.co.uk>
2016-10-06 17:12:37 +0100

    started on probabilities

--- filter.py
+++ filter.py
@@ -12,6 +12,8 @@ def file_names(paths):
         file_list += files
     return file_list
 
+def count_path_total(path):
+        return len(listdir(path))
 
 def is_spam(path):
     items = path.split('/') # assumes that it is in the correct folder
@@ -30,7 +32,7 @@ def count_words(file_list):
     Counts all the words from a group of files
     :param file_list: List of files to be read
     :return count: dictionary of counted words
-                    e.g. {'daily': ['ham', 3]}
+                    e.g. {'daily': [spam_count, ham_count]}
     """
     counts = {}#defaultdict(lambda: [0,0])
     for file_name in file_list:
@@ -52,9 +54,30 @@ def count_words(file_list):
 
     return counts
 
+def word_probabilities(counts, total_spam, total_ham):
+    # CONSTANTS
+    # P(spam)
+    S = total_spam/(total_spam+total_ham*1.0)
+    # P(notspame)
+    notS = total_ham/(total_spam+total_ham*1.0)
+
+    for word, totals in counts.iteritems():
+        # P(word)
+        W = (totals[0])/(total_spam*1.0)
+        print (W*S)/S
+
+
+
+
+
+
+
+ham_total = count_path_total(PATHS[0])
+spam_total = count_path_total(PATHS[1])
 
 counts = count_words(file_names(PATHS))
-print dict(counts)
+
+word_probabilities(counts, spam_total, ham_total)
 
 
 

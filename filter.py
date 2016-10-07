aef89fff8827c7796c718b7ca7f05860f20f0372
mcgeorgiev <michael_georgiev@hotmail.co.uk>
2016-10-07 15:53:00 +0100

    working filter and more emails

--- filter.py
+++ filter.py
@@ -1,4 +1,5 @@
 import re
+import math
 from os import listdir
 from collections import defaultdict
 
@@ -22,9 +23,13 @@ def is_spam(path):
 
 def tokenize(message):
     """Very basic tokenization. Does not get rid of number words and does not deal with the subject/body."""
+    
+    # set() gets distinct values
+    # hacky way to deal with subject line
     message = message.lower()
-    all_words = re.findall("[a-z0-9']+", message)
-    return all_words
+    all_words = set(re.findall("[a-z0-9']+", message))
+    return [word for word in list(all_words) if word != 'subject']
+
 
 
 def count_words(file_list):
@@ -34,7 +39,7 @@ def count_words(file_list):
     :return count: dictionary of counted words
                     e.g. {'daily': [spam_count, ham_count]}
     """
-    counts = {}#defaultdict(lambda: [0,0])
+    counts = defaultdict(lambda: [0,0])
     for file_name in file_list:
         with open(file_name, 'r') as f:
             message = f.read()
@@ -54,30 +59,54 @@ def count_words(file_list):
 
     return counts
 
-def word_probabilities(counts, total_spam, total_ham):
-    # CONSTANTS
-    # P(spam)
-    S = total_spam/(total_spam+total_ham*1.0)
-    # P(notspame)
-    notS = total_ham/(total_spam+total_ham*1.0)
-
-    for word, totals in counts.iteritems():
-        # P(word)
-        W = (totals[0])/(total_spam*1.0)
-        print (W*S)/S
+def word_probabilities(counts, total_spam, total_ham, k=0.5):
+    return [[word, (totals[0]+k)/(total_spam*2*k), (totals[1]+k)/(total_ham*2*k)] for word, totals in counts.iteritems()]
 
 
-
-
-
-
+def product(number_list):
+    prod = 1
+    for n in number_list:
+         prod *= n
+    return prod         
+    
+    
+def spam_probability(word_probabilities, message, spam_total, ham_total):
+    spam_log = 0.0
+    ham_log = 0.0
+    for word, spam_prob, ham_prob in word_probabilities:
+        if word in tokenize(message):
+            #print word, spam_prob, ham_prob
+            spam_log += math.log(spam_prob)
+            ham_log += math.log(ham_prob)
+            
+        else:
+           # print word, spam_prob, ham_prob
+            # print word
+            spam_log += math.log(1.0-spam_prob)
+            ham_log += math.log(1.0-ham_prob)
+        
+    spam = math.exp(spam_log)
+    ham = math.exp(ham_log)
+    return spam/(spam+ham), ham/(spam+ham)
+    
 
 ham_total = count_path_total(PATHS[0])
 spam_total = count_path_total(PATHS[1])
 
 counts = count_words(file_names(PATHS))
 
-word_probabilities(counts, spam_total, ham_total)
+word_probs = word_probabilities(counts, spam_total, ham_total)
+
+message = '''It is truly incredible - People from all around the world are using
+their webcams to get off. Now is your chance to watch Men and women,
+boys and girls show off just for you. Best of all, it's FREE, LIVE
+and UN-F*CKING-BELIEVABLE. Either peep in on the sexy activity or
+participate with your own webcam! You've got to try this out!
+
+
+Open Amateur Webcam Feeds are Active RIGHT NOW!!!'''
+
+print spam_probability(word_probs, message, spam_total, ham_total)
 
 
 
